3de8afab84460677f49666e368fa1fb4b0d6f82b
pa.malkov <pa.malkov@vkteam.ru>
2025-11-28 17:31:29 +0300

    master feat: Implemented deploying scripts

--- utils/production/zksync.sh
+++ utils/production/zksync.sh
@@ -1,1 +1,10 @@
-java -jar zkcopy.jar --source 2a00:b4c0:1c1::1241:0:2181/ichwill-zen/production --target fd00:b4c4:c110:101:1:0:1859:0:2181/ichwill-zen/production -w 40 --syncMode --withMetrics --metricsUrl http://vmagent.mon.one-infra.ru/api/v1/import/prometheus > zksync.log &
+java -jar zkcopy.jar \
+  --source 2a00:b4c0:1c1::1241:0:2181,2a00:b4c0:8c1::1d6e:0:2181,2a00:b4c0:3c1::2552:0:2181/ichwill-zen/production \
+  --target fd00:b4c4:c111:101:1:0:425d:0:2181,fd00:b4c4:c110:101:1:0:1859:0:2181,fd00:b4c4:c106:102:2:0:1cf:0:2181/ichwill-zen/production \
+  -w 40 \
+  --syncMode \
+  --withMetrics \
+  --metricsUrl http://vmagent.mon.one-infra.ru/api/v1/import/prometheus \
+  --cloud PC \
+  --ctype prod \
+    > zksync.log &
--- utils/testing/sync_worker_mismatches.sh
+++ utils/testing/sync_worker_mismatches.sh
@@ -5,7 +5,7 @@ TARGET="fd00:b4c4:c110:101:1:0:1859:0:2181"
 ZKCOPY_JAR="zkcopy.jar"
 JOBS=30
 
-PATHS=(
+PATHS_TO_TARGET=(
 "/ichwill-zen/production/bazinga_ok_social_recommender_worker/heartbeat"
 "/ichwill-zen/production/bazinga_ok_money_worker/heartbeat"
 "/ichwill-zen/production/bazinga_topic_channel_worker/heartbeat"
@@ -36,10 +36,19 @@ PATHS=(
 "/ichwill-zen/production/bazinga_comments_worker/heartbeat"
 )
 
+PATHS_TO_SOURCE=(
+)
+
 echo "Starting parallel sync with $JOBS jobs..."
 
-printf "%s\n" "${PATHS[@]}" | xargs -P "$JOBS" -I {} \
+printf "%s\n" "${PATHS_TO_TARGET[@]}" | xargs -P "$JOBS" -I {} \
     java -jar "$ZKCOPY_JAR" \
     --source "$SOURCE{}" \
     --target "$TARGET{}" \
+    --workers 10
+
+printf "%s\n" "${PATHS_TO_SOURCE[@]}" | xargs -P "$JOBS" -I {} \
+    java -jar "$ZKCOPY_JAR" \
+    --source "$TARGET{}" \
+    --target "$SOURCE{}" \
     --workers 10
